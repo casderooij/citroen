3b05bbef5707fa7ba7e63dd460f0a89ca4c0d945
casderooij <hello@casderooij.nl>
2022-03-22 23:37:30 +0100

    Initialise Citroen theme

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./**/*.liquid"],
+  theme: {
+		container: {
+			center: true
+		},
+		fontFamily: {
+			'sans': ['Necto', 'ui-monospace']
+		},
+		extend: {},
+  },
+  plugins: [],
+}
